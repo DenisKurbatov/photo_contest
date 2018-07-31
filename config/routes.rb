90f7192b0ef9fc5d39e2cfea5d757e4a9a51a2ea
Denis Kurbatov <kurbatov.work@gmail.com>
2018-07-31 14:48:31 +0300

    vk-auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users, controllers: { omniauth_callbacks: 'sessions' }
-  root 'static_pages#home'
-  get '/home', to: 'static_pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
   devise_scope :user do
     delete 'logout', to: 'sessions#destroy'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
