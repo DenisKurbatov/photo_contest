7a346ce833f739c6eb93fd384f002b6f0fafa9f1
Denis Kurbatov <kurbatov.work@gmail.com>
2018-08-17 15:50:55 +0300

    current

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < Devise::OmniauthCallbacksController
   def vkontakte
     @user = User.from_omniauth(request.env["omniauth.auth"])
+  
     if @user.persisted?
       session[:user_id] = @user.id
     end
@@ -9,6 +10,7 @@ class SessionsController < Devise::OmniauthCallbacksController
 
   def destroy
     session[:user_id] = nil
+    @current_user = nil
     redirect_to root_path
   end
 end
