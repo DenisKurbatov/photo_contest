d2cfa1eb4e879e9ef3320fb188ec6ea765d94cf2
Denis Kurbatov <kurbatov.work@gmail.com>
2018-09-27 22:36:16 +0300

    добавление/удалени лайков в галерее ( Ajax)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,43 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+  $(".like").on('click', '.add-like', function(){
+    $.ajax({
+      url: "/photos/" + $(this).parent().attr("id") + "/likes",
+      type: "POST",
+      dataType: "json",
+      success: function(res) {
+
+        $("#" + res.photo_id + " .add-like").replaceWith("<span class='remove-like glyphicon glyphicon-heart red' data-like-id='" + res.like_id+"'></span>");
+        $("#" + res.photo_id + " .count-like").replaceWith("<span class='count-like'>" + res.likes_count + "</span>");
+
+      }
+    });
+
+  });
+
+  $(".like").on("click", ".remove-like", function(){
+    $.ajax({
+      url: "/photos/" + $(this).parent().attr("id") + "/likes/" + $(this).attr("data-like-id"),
+      type: "POST",
+      dataType: "json",
+      data: { _method: "DELETE" },
+      success: function(res) {
+        console.log(res);
+
+        console.log(res.likes_count);
+        $("#" + res.photo_id + " .remove-like").replaceWith("<span class='add-like glyphicon glyphicon-heart blue'></span>");
+        $("#" + res.photo_id + " .count-like").replaceWith("<span class='count-like'>" + res.likes_count + "</span>");
+      }
+    });
+
+
+  });
+
+  
+
+  
+
+});
+
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,13 @@
 class LikesController < ApplicationController
 
   def create
-    photo = Photo.find(params[:photo_id])
-    @like = Like.new(photo: photo, user: current_user)
+    @photo = Photo.find(params[:photo_id])
+    @like = Like.new(photo: @photo, user: current_user)
     respond_to do |format|
       if @like.save
-        format.html { redirect_to photo }
-        format.js
+        format.html { redirect_to @photo }
+        format.json { render json: { photo_id: @photo.id, likes_count: @photo.likes_count, like_id: @like.id } }
       else
-        
       end
     end
   end
@@ -16,7 +15,12 @@ class LikesController < ApplicationController
   def destroy
     photo = Photo.find(params[:photo_id])
     like = Like.find_by(photo: photo, user: current_user)
-    like.destroy
-    redirect_to photo
+    
+    respond_to do |format|
+      if like.destroy
+        format.json { render json: { photo_id: photo.id, likes_count: photo.likes_count-1 } }
+        format.html { redirect_to photo }
+      end
+    end
   end
 end
