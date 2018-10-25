8efb9a9979410804a724c8f68ceece56cf6e345c
Denis Kurbatov <kurbatov.work@gmail.com>
2018-10-25 14:43:26 +0300

    -

--- config/initializers/leaderbord.rb
+++ config/initializers/leaderbord.rb
@@ -2,5 +2,5 @@ require 'leaderboard'
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:url => uri)
 redis_options = {:redis_connection => REDIS}
-LikesRating = Leaderboard.new('highscores')
+
 PastState = Leaderboard.new('paststate', Leaderboard::DEFAULT_OPTIONS, redis_options)
