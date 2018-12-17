c6abced04471bde053486bd821e64428a81ed64e
Denis Kurbatov <kurbatov.work@gmail.com>
2018-12-18 02:11:35 +0300

    исправлена страница фото пользователя

--- app/controllers/users/photos_controller.rb
+++ app/controllers/users/photos_controller.rb
@@ -1,11 +1,7 @@
 module Users
   class PhotosController < ApplicationController
     def index
-      if current_user.id == params[:user_id].to_i
-        @photos = Photo.by_user(params[:user_id]).page params[:page]
-      else
-        redirect_to user_photos_path(current_user)
-      end
+      @photos = ListPhotos.run!(user_id: current_user.id)
     end
   end
 end
--- app/interactions/photos/list_photos.rb
+++ app/interactions/photos/list_photos.rb
@@ -1,10 +1,15 @@
 class ListPhotos < ActiveInteraction::Base
-  string :sorting
+  string :sorting, default: 'id asc'
   string :search, default: nil
+  integer :user_id, default: nil
 
   def execute
-    photos = Photo.approved.order(sorting)
-    photos = photos.where("name ILIKE '%#{search}%'") unless search.nil?
+    if user_id
+      photos = Photo.find_by(user_id: user_id)
+    else
+      photos = Photo.approved.order(sorting)
+      photos = photos.where("name ILIKE '%#{search}%'") unless search.nil?
+    end
     photos
   end
 end
